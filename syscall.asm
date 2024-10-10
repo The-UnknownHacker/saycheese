2d7b994a1160c210ba22a718ef3ade9e4f531929
commonkestrel <jettb6@gmail.com>
2024-10-10 12:30:30 -0700

    unbuffered stdin

--- syscall.asm
+++ syscall.asm
@@ -2,12 +2,19 @@
 %define SYSCALL_ASM
 
 %define WRITE 1
+%define IOCTL 16
 %define EXIT  60
 
 %macro DEF_STR 2+
     %1 db %2
     %1_len equ $-%1
 %endmacro
+
+struc termios
+    resb 12
+    .flags: resb 12
+    resb 44
+endstruc
 
 section .text
 
@@ -17,6 +24,43 @@ write:
     mov rax, WRITE
     syscall
     ret
+
+global read
+
+read:
+    mov rax, 0
+    syscall
+
+
+global ioctl
+
+; get struct
+%define TCGETS 21505 
+; set struct   
+%define TCSETS 21506  
+
+; rax: termios struct pointer
+; rdx: 0 - get, 1 - set
+ioctl:
+    push rdi
+    push rsi
+
+    add rdx, TCGETS
+    mov rsi, rdx
+    mov rdx, rax
+    mov rdi, 0
+    mov rax, IOCTL
+    syscall
+
+    pop rsi
+    pop rdi
+    ret
+
+global exit
+
+exit:
+    mov rax, EXIT
+    syscall
 
 %endif
     
