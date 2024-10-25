d1c6975004863b8a3aac562a27750298fc48c141
commonkestrel <jettb6@gmail.com>
2024-10-25 09:38:54 -0700

    working nix config :)

--- examples/asm/flake.nix
+++ examples/asm/flake.nix
@@ -5,8 +5,16 @@
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       {
-        packages.${system} = (import ./shell.nix)
-            { pkgs = nixpkgs.legacyPackages.${system}; };
+        defaultPackage = let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        pkgs.mkShell {
+          buildInputs = [
+            pkgs.gcc  
+            pkgs.nasm
+            pkgs.qrencode
+          ];
+        };
       }
     );
 }
